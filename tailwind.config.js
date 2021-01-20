980342d3f0467436b05f444fa79fba02aad077f9
Gijs Burghoorn <g.burghoorn@gmail.com>
2021-01-20 12:28:52 +0100

    Almost finished index. Added theme and presets.
    Index page:
    - Added Newest Update to index page
    - Improved styling of navbar
    - Updated all classes to use theming
    
    Theme, presets & settings:
    - Added theme colors, font.
    - Added presents for squished container, card, menu-card, content,
    header, ref and seperator-down.
    - Added purge settings & tested it.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 module.exports = {
-	purge: ["./index.html", "./src/**/*.html", "./src/**/*.js"],
-	darkMode: false, // or 'media' or 'class'
+	purge: { enabled: false, content: ["./**/*.html"] },
+	darkMode: false,
 	theme: {
-		extend: {},
+		screens: {
+			screens: {
+				sm: "640px",
+				md: "768px",
+				lg: "1024px",
+				xl: "1280px",
+				"2xl": "1536px",
+			},
+		},
+		extend: {
+			fontFamily: {
+				content: ["Helvetica", "sans-serif"],
+				header: ["Verdana", "sans-serif"],
+			},
+			colors: {
+				bgcolor: "#E6E6E6",
+				titlebgcolor: "#FFFFFF",
+				containerhl: "#B6B6B6",
+				seperatorcolor: "#EFEFEF",
+				headercolor: "#000000",
+				refcolor: "#479BF3",
+				contentcolor: "#7E7E7E",
+			},
+		},
 	},
 	variants: {
 		extend: {},
