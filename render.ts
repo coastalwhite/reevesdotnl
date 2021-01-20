b827de9062e2871f9fe27b893eb4486b98ca5111
Gijs Burghoorn <g.burghoorn@gmail.com>
2021-01-20 15:36:08 +0100

    Modularized the News components.
    Moved all the current news items to a news excerpt in the src/news/items
    folder. This way items can be easy added or removed.

--- render.ts
+++ render.ts
@@ -2,28 +2,32 @@
 import denjucks from "https://deno.land/x/bq_denjucks@1.1.2/mod.js";
 
 const exportFiles = [
-	{ fileName: "index.html", data: { home: true } },
-	{ fileName: "gondwana.html", data: { gondwana: true } },
-	{ fileName: "news.html", data: { news: true } },
-	{ fileName: "publications.html", data: { publications: true } },
-	{ fileName: "about.html", data: { about: true } },
-	{ fileName: "contact.html", data: { contact: true } },
+	{ fileName: "index.html", path: "", data: { home: true } },
+	{ fileName: "gondwana.html", path: "", data: { gondwana: true } },
+	{ fileName: "index.html", path: "news/", data: { news: true } },
+	{
+		fileName: "publications.html",
+		path: "",
+		data: { publications: true },
+	},
+	{ fileName: "about.html", path: "", data: { about: true } },
+	{ fileName: "contact.html", path: "", data: { contact: true } },
 ];
 
-exportFiles.forEach(({ fileName, data }) => {
+exportFiles.forEach(({ fileName, path, data }) => {
 	console.log("Starting with %s", fileName);
-	const compiledHTML = denjucks.render("./src/" + fileName, data);
+	const compiledHTML = denjucks.render("./src/" + path + fileName, data);
 	console.log("Compiled %s into HTML", fileName);
-	const fileWrite = Deno.writeTextFile("./" + fileName, compiledHTML);
+	const fileWrite = Deno.writeTextFile("./" + path + fileName, compiledHTML);
 	fileWrite
 		.then(() =>
-			console.log("Wrote %s to './%s'", fileName, fileName)
+			console.log("Wrote %s to './%s'", path + fileName, path + fileName)
 		)
 		.catch((err) =>
 			console.log(
 				"Failed to write %s to './%s', reason: %s",
-				fileName,
-				fileName,
+				path + fileName,
+				path + fileName,
 				err
 			)
 		);
