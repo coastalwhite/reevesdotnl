2bb09f80cbe7ed65bd011f1276fdda344af9fc54
Gijs Burghoorn <g.burghoorn@gmail.com>
2021-03-30 11:07:25 +0200

    Made gondwana main text adjustable

---
+++ src/md-texts/gondwana.md
@@ -0,0 +1,25 @@
+---
+title: "Links"
+animation_path: "img/gondwana.gif"
+animation_alt: "Gondwana"
+---
+
+How did Gondwana break up exactly?  The southern continents – more than half the
+world’s land area – share much geological and tectonic history.  The formation
+of Gondwana as a ‘supercontinent’ was completed at the start of Phanerozoic
+times, about 540 million years ago (540 Ma). It remained intact as a single
+large landmass for two-thirds of the time between then and now, eventually
+starting to fragment in the Jurassic period, about 184 Ma. During Early
+Cretaceous times, especially in the interval 140-120 Ma, most of the present
+continental outlines were established and these fragments started moving apart
+to give the familiar geography of today with almost half of the world’s oceans
+now separating them. The research presented here has attempted to define step by
+step the history of this dispersal against the geological time-scale and in
+geometrical detail. While evidence of all sorts has been used, it is the careful
+interpretation of the topography of the sea floor, revealed in detail by
+satellite gravimetry only since about 1997, that has been key to this new
+understanding. The continental margins formed in the process - and the rifts
+that have penetrated but not separated the present-day continents - are of
+immense economic importance.  The links below lead to a large resource of
+information from the principles of global tectonics by way of graphic animations
+and published texts to the latest pre-publication research ideas.
--- src/pages/gondwana/index.tsx
+++ src/pages/gondwana/index.tsx
@@ -4,10 +4,11 @@ import RelatedLink from "../../components/related-link";
 import CoverImageLayout from "../../components/layouts/cover-image";
 import TitledCard from "../../components/cards/titled-card";
 import { useStaticQuery, graphql } from "gatsby";
+import { LoadWithMD } from "../../md-graphql";
 
-const text =
-    "How did Gondwana break up exactly?  The southern continents – more than half the world’s land area – share much geological and tectonic history.  The formation of Gondwana as a ‘supercontinent’ was completed at the start of Phanerozoic times, about 540 million years ago (540 Ma). It remained intact as a single large landmass for two-thirds of the time between then and now, eventually starting to fragment in the Jurassic period, about 184 Ma. During Early Cretaceous times, especially in the interval 140-120 Ma, most of the present continental outlines were established and these fragments started moving apart to give the familiar geography of today with almost half of the world’s oceans now separating them. The research presented here has attempted to define step by step the history of this dispersal against the geological time-scale and in geometrical detail. While evidence of all sorts has been used, it is the careful interpretation of the topography of the sea floor, revealed in detail by satellite gravimetry only since about 1997, that has been key to this new understanding. The continental margins formed in the process - and the rifts that have penetrated but not separated the present-day continents - are of immense economic importance.  The links below lead to a large resource of information from the principles of global tectonics by way of graphic animations and published texts to the latest pre-publication research ideas.";
-
+interface FrontmatterProps {
+    title: string;
+}
 const GondwanaPage = () => {
 
     const data = useStaticQuery(graphql`
@@ -23,32 +24,33 @@ const GondwanaPage = () => {
         }
     `)
     const principles = data.allFile.edges.map(x => x.node).find(x => x.name.match("principles of global"));
-    return (
-        <CoverImageLayout
-            pageName="gondwana"
-            pageTitle="Gondwana"
-            coverImgSrc="/assets/img/gondwana.gif"
-            coverImgAlt="gondwana"
-        >
-            <TitledCard title="Links">
-                <Text mb="4">
-                    {text}
-                </Text>
+    return (LoadWithMD<FrontmatterProps>(
+        "gondwana.md",
+        (html, fm) => (
+            <CoverImageLayout
+                pageName="gondwana"
+                pageTitle="Gondwana"
+                coverImgSrc="/assets/img/gondwana.gif"
+                coverImgAlt="Gondwana"
+            >
+                <TitledCard title={fm.title}>
+                    <Text mb="4" dangerouslySetInnerHTML={{ __html: html }} />
 
-                <RelatedLink
-                    to={principles.publicURL}
-                    text="Principles of global tectonics"
-                />
-                <RelatedLink
-                    to="/gondwana/animations"
-                    text="Selected animations"
-                />
-                <RelatedLink to="/gondwana/updates" text="Research updates" />
-                <RelatedLink to="/gondwana" text="Twelve lectures" />
-                <RelatedLink to="/gondwana" text="Other material" />
-            </TitledCard>
-        </CoverImageLayout>
-    );
+                    <RelatedLink
+                        to={principles.publicURL}
+                        text="Principles of global tectonics"
+                    />
+                    <RelatedLink
+                        to="/gondwana/animations"
+                        text="Selected animations"
+                    />
+                    <RelatedLink to="/gondwana/updates" text="Research updates" />
+                    <RelatedLink to="/gondwana" text="Twelve lectures" />
+                    <RelatedLink to="/gondwana" text="Other material" />
+                </TitledCard>
+            </CoverImageLayout>
+        )
+    ));
 };
 
 export default GondwanaPage;
