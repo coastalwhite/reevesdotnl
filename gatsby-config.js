97a6ef83c7c896fc36c2f6ae8e1978bbb8e52f01
Gijs Burghoorn <g.burghoorn@gmail.com>
2021-04-08 18:04:45 +0200

    Added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,69 +1,91 @@
 module.exports = {
-  pathPrefix : "/reevesdotnl",
-  siteMetadata : {
-    title : "Reeves.nl",
+  pathPrefix: "/reevesdotnl",
+  siteMetadata: {
+    title: "Reeves.nl",
     siteUrl: "http://www.reeves.nl/",
-    author : "Colin Reeves",
-    keywords : [
-      "Geophysics", "Geological mapping", "Southern continents", "Gondwana",
-      "Geodynamics", "Aeromagnetic surveys", "Geophysical interpretation",
-      "Ocean-floor topography", "Paleogeographic reconstructions",
+    author: "Colin Reeves",
+    keywords: [
+      "Geophysics",
+      "Geological mapping",
+      "Southern continents",
+      "Gondwana",
+      "Geodynamics",
+      "Aeromagnetic surveys",
+      "Geophysical interpretation",
+      "Ocean-floor topography",
+      "Paleogeographic reconstructions",
       "Africa, India, Australia, Antarctica, South America",
-      "Cretaceous period", "Jurassic period", "Continental margins", "Rifts",
-      "Mid-ocean ridges", "Dyke swarms", "Large igneous provinces",
-      "Precambrian terranes"
-    ]
+      "Cretaceous period",
+      "Jurassic period",
+      "Continental margins",
+      "Rifts",
+      "Mid-ocean ridges",
+      "Dyke swarms",
+      "Large igneous provinces",
+      "Precambrian terranes",
+    ],
   },
-  plugins : [
-    "gatsby-plugin-sharp", "gatsby-plugin-react-helmet", {
-      resolve : "@chakra-ui/gatsby-plugin",
-      options : {
-        isResettingCSS : true,
-        isUsingColorMode : true,
+  plugins: [
+    {
+      resolve: `gatsby-plugin-goatcounter`,
+      options: {
+        // Either `code` or `selfHostUrl` is required.
+        // REQUIRED IF USING HOSTED GOATCOUNTER! https://[my_code].goatcounter.com
+        code: "reeves",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "@chakra-ui/gatsby-plugin",
+      options: {
+        isResettingCSS: true,
+        isUsingColorMode: true,
       },
     },
     {
-      resolve : "gatsby-source-filesystem",
-      options : {name : 'pages', path : `${__dirname}/src/pages/`}
+      resolve: "gatsby-source-filesystem",
+      options: { name: "pages", path: `${__dirname}/src/pages/` },
     },
     {
-      resolve : "gatsby-source-filesystem",
-      options : {
-        name : 'data',
-        path : `${__dirname}/src/md-texts/`,
-        ignore : [ "**/\.*" ]
-      }
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "data",
+        path: `${__dirname}/src/md-texts/`,
+        ignore: ["**/\.*"],
+      },
     },
     {
-      resolve : "gatsby-source-filesystem",
-      options : {
-        name : 'assets',
-        path : `${__dirname}/src/assets/`,
-        ignore : [ "**/\.*" ]
-      }
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "assets",
+        path: `${__dirname}/src/assets/`,
+        ignore: ["**/\.*"],
+      },
     },
-    'gatsby-plugin-sharp', {
-      resolve : 'gatsby-transformer-remark',
-      options : {
-        plugins : [ {
-          resolve : `gatsby-remark-copy-linked-files`,
-          options : {ignoreFileExtensions : []}
-        } ]
-      }
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [{
+          resolve: `gatsby-remark-copy-linked-files`,
+          options: { ignoreFileExtensions: [] },
+        }],
+      },
     },
     {
-      resolve : 'gatsby-plugin-mdx',
-      options : {
-        defaultLayouts : {
-          default : require.resolve("./src/templates/pdf-view.tsx"),
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/templates/pdf-view.tsx"),
         },
-        gatsbyRemarkPlugins : [ {
-          resolve : 'gatsby-remark-copy-linked-files',
-          options : {
-            ignoreFileExtensions : [],
-          }
-        } ]
-      }
-    }
+        gatsbyRemarkPlugins: [{
+          resolve: "gatsby-remark-copy-linked-files",
+          options: {
+            ignoreFileExtensions: [],
+          },
+        }],
+      },
+    },
   ],
 };
