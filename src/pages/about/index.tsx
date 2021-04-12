ff6afe98b38e7af46d5d50d6b81ff488088b8daa
Gijs Burghoorn <g.burghoorn@gmail.com>
2021-04-12 11:38:27 +0200

    Fixed about readmore links

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -5,15 +5,13 @@ import { graphql, Link as GatsbyLink } from "gatsby";
 import TitledCard from "../../components/cards/titled-card";
 import SidePicturedLayout from "../../components/layouts/side-pictured";
 
-const Section = (
-  props: { title: string; html: string; pageName: string },
-) => {
+const Section = (props: { title: string; html: string; pageName: string }) => {
   return (
     <Box w="100%" mb="4">
       <TitledCard title={props.title}>
         <Box dangerouslySetInnerHTML={{ __html: props.html }} />
         <Box>
-          <Link color="refcolor" href={props.pageName}>
+          <Link color="refcolor" href={"." + props.pageName}>
             Read more...
           </Link>
         </Box>
@@ -23,29 +21,33 @@ const Section = (
 };
 
 const AboutPage = (props: { data: QueryReturnType }) => {
-  const articles = props.data.allMarkdownRemark.edges.map((x) => x.node).filter(
-    (
-      { fileAbsolutePath },
-    ) => fileAbsolutePath.match(/aboutpage\//),
-  ).map(({ html, frontmatter: fm }) => ({
-    html,
-    title: fm.title,
-    pageName: fm.readmore_uri,
-  }));
+  const articles = props.data.allMarkdownRemark.edges
+    .map((x) => x.node)
+    .filter(({ fileAbsolutePath }) => fileAbsolutePath.match(/aboutpage\//))
+    .map(({ html, frontmatter: fm }) => ({
+      html,
+      title: fm.title,
+      pageName: fm.readmore_uri,
+    }));
 
-  articles.sort(({ title: titA }, { title: titB }) => titA < titB ? -1 : 0);
+  articles.sort(({ title: titA }, { title: titB }) => (titA < titB ? -1 : 0));
 
   return (
     <SidePicturedLayout
       pageName="about"
       pageTitle="About"
-      images={[{ src: "img/personal.jpg", alt: "news" }, {
-        src: "img/magnetic-anomalies.bmp",
-        alt: "animation",
-      }]}
+      images={[
+        { src: "img/personal.jpg", alt: "news" },
+        {
+          src: "img/magnetic-anomalies.bmp",
+          alt: "animation",
+        },
+      ]}
     >
       <Flex direction="column" flex="1">
-        {articles.map((article, index) => <Section key={index} {...article} />)}
+        {articles.map((article, index) => (
+          <Section key={index} {...article} />
+        ))}
       </Flex>
     </SidePicturedLayout>
   );
@@ -67,20 +69,20 @@ type QueryReturnType = {
 };
 
 export const query = graphql`
-    query AboutExcerpts {
-        allMarkdownRemark {
-            edges {
-                node {
-                    html
-                    frontmatter {
-                        title
-                        readmore_uri
-                    }
-                    fileAbsolutePath
-                }
-            }
+  query AboutExcerpts {
+    allMarkdownRemark {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+            readmore_uri
+          }
+          fileAbsolutePath
         }
+      }
     }
+  }
 `;
 
 export default AboutPage;
