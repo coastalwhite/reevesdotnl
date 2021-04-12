f4ee64b9889bf41330f404e14292ac0bb88e3404
Gijs Burghoorn <g.burghoorn@gmail.com>
2021-04-12 11:36:53 +0200

    Fixed news article readmore links

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -5,79 +5,88 @@ import { graphql } from "gatsby";
 import TitledCard from "../../components/cards/titled-card";
 import SidePicturedLayout from "../../components/layouts/side-pictured";
 
-const ArticlePreview = (
-    props: { title: string; html: string; pageName: string },
-) => {
-    return (
-        <Box w="100%" mb="4">
-            <TitledCard title={props.title}>
-                <Box dangerouslySetInnerHTML={{ __html: props.html }} />
-                <Box>
-                    <Link color="refcolor" href={props.pageName}>
-                        Read more...
+const ArticlePreview = (props: {
+  title: string;
+  html: string;
+  pageName: string;
+}) => {
+  return (
+    <Box w="100%" mb="4">
+      <TitledCard title={props.title}>
+        <Box dangerouslySetInnerHTML={{ __html: props.html }} />
+        <Box>
+          <Link color="refcolor" href={"news/" + props.pageName}>
+            Read more...
           </Link>
-                </Box>
-            </TitledCard>
-        </Box >
-    );
+        </Box>
+      </TitledCard>
+    </Box>
+  );
 };
 
 const NewsPage = (props: { data: QueryReturnType }) => {
-    const articles = props.data.allMdx.edges.map((x) => x.node)
-        .sort(({ frontmatter: a }, { frontmatter: b }) => (a.date == b.date ? 0 : (a.date < b.date ? 1 : -1))).map(({ excerpt, slug, frontmatter: fm }) => ({
-            html: excerpt,
-            title: fm.title,
-            pageName: slug,
-        }));
+  const articles = props.data.allMdx.edges
+    .map((x) => x.node)
+    .sort(({ frontmatter: a }, { frontmatter: b }) =>
+      a.date == b.date ? 0 : a.date < b.date ? 1 : -1
+    )
+    .map(({ excerpt, slug, frontmatter: fm }) => ({
+      html: excerpt,
+      title: fm.title,
+      pageName: slug.substr(slug.lastIndexOf("/") + 1),
+    }));
 
-    return (
-        <SidePicturedLayout
-            pageName="news"
-            pageTitle="News"
-            images={[{ src: "img/news.jpg", alt: "news" }, {
-                src: "img/animation.gif",
-                alt: "animation",
-            }]}
-        >
-            <Flex direction="column" flex="1">
-                {articles.map((article, index) =>
-                    <ArticlePreview key={index} {...article} />
-                )}
-            </Flex>
-        </SidePicturedLayout>
-    );
+  return (
+    <SidePicturedLayout
+      pageName="news"
+      pageTitle="News"
+      images={[
+        { src: "img/news.jpg", alt: "news" },
+        {
+          src: "img/animation.gif",
+          alt: "animation",
+        },
+      ]}
+    >
+      <Flex direction="column" flex="1">
+        {articles.map((article, index) => (
+          <ArticlePreview key={index} {...article} />
+        ))}
+      </Flex>
+    </SidePicturedLayout>
+  );
 };
 
 type QueryReturnType = {
-    allMdx: {
-        edges: {
-            node: {
-                excerpt: string;
-                slug: string;
-                frontmatter: {
-                    date: string;
-                    title: string;
-                };
-            };
-        }[];
-    };
+  allMdx: {
+    edges: {
+      node: {
+        excerpt: string;
+        slug: string;
+        frontmatter: {
+          date: string;
+          title: string;
+        };
+      };
+    }[];
+  };
 };
 
 export const query = graphql`
-    query NewsArticles {
-        allMdx(filter: {slug: {regex: "/news/"}}) {
-            edges {
-              node {
-                frontmatter {
-                  title
-                  date
-                }
-                excerpt(pruneLength: 400)
-                slug
-              }
-            }
+  query NewsArticles {
+    allMdx(filter: { slug: { regex: "/news/" } }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
           }
+          excerpt(pruneLength: 400)
+          slug
+        }
+      }
     }
+  }
 `;
 
 export default NewsPage;
