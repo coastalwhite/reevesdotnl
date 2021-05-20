c11784ef3ad35adbde322520cc831b626564434f
Gijs Burghoorn <g.burghoorn@gmail.com>
2021-05-20 15:23:17 +0200

    Changed news images
    * Removed old news images
    * Added new news image

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -6,70 +6,66 @@ import TitledCard from "../../components/cards/titled-card";
 import SidePicturedLayout from "../../components/layouts/side-pictured";
 
 const ArticlePreview = (props: {
-  title: string;
-  html: string;
-  pageName: string;
+    title: string;
+    html: string;
+    pageName: string;
 }) => {
-  return (
-    <Box w="100%" mb="4">
-      <TitledCard title={props.title}>
-        <Box dangerouslySetInnerHTML={{ __html: props.html }} />
-        <Box>
-          <GatsbyLink to={props.pageName}>
-            <Link color="refcolor">Read more...</Link>
-          </GatsbyLink>
+    return (
+        <Box w="100%" mb="4">
+            <TitledCard title={props.title}>
+                <Box dangerouslySetInnerHTML={{ __html: props.html }} />
+                <Box>
+                    <GatsbyLink to={props.pageName}>
+                        <Link color="refcolor">Read more...</Link>
+                    </GatsbyLink>
+                </Box>
+            </TitledCard>
         </Box>
-      </TitledCard>
-    </Box>
-  );
+    );
 };
 
 const NewsPage = (props: { data: QueryReturnType }) => {
-  const articles = props.data.allMdx.edges
-    .map((x) => x.node)
-    .sort(({ frontmatter: a }, { frontmatter: b }) =>
-      a.date == b.date ? 0 : a.date < b.date ? 1 : -1
-    )
-    .map(({ excerpt, slug, frontmatter: fm }) => ({
-      html: excerpt,
-      title: fm.title,
-      pageName: slug.substr(slug.lastIndexOf("/") + 1),
-    }));
+    const articles = props.data.allMdx.edges
+        .map((x) => x.node)
+        .sort(({ frontmatter: a }, { frontmatter: b }) =>
+            a.date == b.date ? 0 : a.date < b.date ? 1 : -1
+        )
+        .map(({ excerpt, slug, frontmatter: fm }) => ({
+            html: excerpt,
+            title: fm.title,
+            pageName: slug.substr(slug.lastIndexOf("/") + 1),
+        }));
 
-  return (
-    <SidePicturedLayout
-      pageName="news"
-      pageTitle="News"
-      images={[
-        { src: "img/news.jpg", alt: "news" },
-        {
-          src: "img/animation.gif",
-          alt: "animation",
-        },
-      ]}
-    >
-      <Flex direction="column" flex="1">
-        {articles.map((article, index) => (
-          <ArticlePreview key={index} {...article} />
-        ))}
-      </Flex>
-    </SidePicturedLayout>
-  );
+    return (
+        <SidePicturedLayout
+            pageName="news"
+            pageTitle="News"
+            images={[
+                { src: "img/news.jpg", alt: "news" },
+            ]}
+        >
+            <Flex direction="column" flex="1">
+                {articles.map((article, index) => (
+                    <ArticlePreview key={index} {...article} />
+                ))}
+            </Flex>
+        </SidePicturedLayout>
+    );
 };
 
 type QueryReturnType = {
-  allMdx: {
-    edges: {
-      node: {
-        excerpt: string;
-        slug: string;
-        frontmatter: {
-          date: string;
-          title: string;
-        };
-      };
-    }[];
-  };
+    allMdx: {
+        edges: {
+            node: {
+                excerpt: string;
+                slug: string;
+                frontmatter: {
+                    date: string;
+                    title: string;
+                };
+            };
+        }[];
+    };
 };
 
 export const query = graphql`
