12c57ce355c810f5d83cfa1d47b841fb108535f9
coastalwhite <me@gburghoorn.com>
2024-06-10 19:17:50 +0200

    add google search inspection

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -22,6 +22,7 @@ const Base = (props: BaseProps) => {
                 <meta name="keywords" content=" Geophysics, Geological mapping, Southern continents, Gondwana, Geodynamics, Aeromagnetic surveys, Geophysical interpretation, Ocean-floor topography, Paleogeographic reconstructions, Africa, India, Australia, Antarctica, South America, Cretaceous period, Jurassic period, Continental margins, Rifts, Mid-ocean ridges, Dyke swarms, Large igneous provinces, Precambrian terranes" />
                 <meta name="author" content="Colin Reeves" />
                 <meta name="description" content="Colin Reeves MA MSc PhD. Achterom 41a 2611 PL Delft The Netherlands. +31 611 35 62 72. reeves.earth@planet.nl. Earthworks B.V.. Lange Geer 44" />
+                <meta name="google-site-verification" content="MJJ4RwHHjbiqaRbH4r0Bu9P_Md7y8d33DcAWaVlItJo" />
                 <title>Reeves.nl / Earthworks</title>
             </Helmet>
             <Menu enabled={props.pageName} />
