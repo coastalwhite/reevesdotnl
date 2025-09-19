b29a526023571b5e5654c1845af217a8813f4e93
reevesdotnl <reevesdotnl@users.noreply.github.com>
2025-09-19 10:31:51 +0000

    deployed via Gatsby Publish Action 🎩 for 5ee5ad7264286430b3f5a34c5c74615017b99687

---
+++ static/830aa468a7d03633c024f78d7c6cda7d/index.tsx
@@ -0,0 +1,26 @@
+import { Box, Flex } from "@chakra-ui/react";
+import * as React from "react";
+import LatestUpdateCard from "../components/cards/latest-update";
+import ShortAboutCard from "../components/cards/short-about";
+import CoverImageLayout from "../components/layouts/cover-image";
+
+const IndexPage = () => {
+    return (
+        <CoverImageLayout
+            pageName="homepage"
+            coverImgSrc="/assets/img/homepage.gif"
+            coverImgAlt="Gondwana"
+        >
+            <Flex direction="column" maxW="100vw">
+                <Box mb="4">
+                    <ShortAboutCard />
+                </Box>
+                <Box>
+                    <LatestUpdateCard />
+                </Box>
+            </Flex>
+        </CoverImageLayout>
+    );
+};
+
+export default IndexPage;
